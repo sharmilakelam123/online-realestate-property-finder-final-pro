083882edb84d6f58509585a6fd4939347f2b8e55
sharmilakelam123 <sharmilakelam1998@gmail.com>
2026-09-17 19:10:09 +0530

    Fix API backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://realestate-pro-2.onrender.com",
+  baseURL: "https://online-realestate-property-finder-final-rdfh.onrender.com",
 });
 
 export default API;
